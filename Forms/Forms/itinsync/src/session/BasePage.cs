7d03fafcdbf723e48d8dac4c2468ae8fea3c96cb
Qundeel Ch <Qundeel Ch@DESKTOP-JNTNUBE>
2017-05-10 23:45:19 -0700

    commit

--- Forms/Forms/itinsync/src/session/BasePage.cs
+++ Forms/Forms/itinsync/src/session/BasePage.cs
@@ -894,13 +894,16 @@ namespace Forms.itinsync.src.session
 
         public Int32 getLastVisitedURLCode()
         {
-            if (((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK)).Count == 0)
-                return 0;
-            else if (((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK)).Count == 1)
-                return ((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK))[0];
+            if (Sessions.getSession().Get(SessionKey.PAGEVISTSTACK) != null)
+            {
+                if (((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK)).Count == 0)
+                    return 0;
+                else if (((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK)).Count == 1)
+                    return ((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK))[0];
 
-            return ((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK))[((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK)).Count - 1];
-
+                return ((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK))[((List<Int32>)Sessions.getSession().Get(SessionKey.PAGEVISTSTACK)).Count - 1];
+            }
+            return 0;
         }
         public List<Int32> getVisitedStack()
         {
