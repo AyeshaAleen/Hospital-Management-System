538076bab7c45140b6e2a534c316678003179da2
itinsync <services@itinsync.com>
2017-06-14 21:25:55 +0500

    fix subject id issue

--- Forms/Forms/Webroot/Forms/Management/ViewDocumentSection.aspx.cs
+++ Forms/Forms/Webroot/Forms/Management/ViewDocumentSection.aspx.cs
@@ -162,7 +162,7 @@ namespace Forms.Webroot.Forms.Management
             DocumentDTO dto = new DocumentDTO();
             dto.header = getHeader();
 
-            dto.documentRole.xdocumentdefinitionid = Convert.ToInt32(getSubjectID());
+            dto.documentRole.xdocumentdefinitionid = getParentRef().getParentrefKey();
             dto.documentRole.role = Convert.ToInt32(ddlUserRole.SelectedValue);
 
             IResponseHandler response = new DocumentRoleSaveService().executeAsPrimary(dto);
@@ -206,7 +206,7 @@ namespace Forms.Webroot.Forms.Management
         protected void btnAddUserEmailRouting_Click(object sender, EventArgs e)
         {
             DocumentDTO dto = new DocumentDTO();
-            dto.documentRouteUsers.xdocumentdefinitionid = Convert.ToInt32(getSubjectID());
+            dto.documentRouteUsers.xdocumentdefinitionid = getParentRef().getParentrefKey();
                 dto.documentRouteUsers.role = Convert.ToInt32(ddlEmailRouting.SelectedValue);
                 dto.documentRouteUsers.userid = Convert.ToInt32(ddlUsers.SelectedValue);
 
@@ -227,7 +227,7 @@ namespace Forms.Webroot.Forms.Management
         {
             DocumentDTO dto = new DocumentDTO();
             dto.header = getHeader();
-                dto.documentRoute.xdocumentdefinitionid = Convert.ToInt32(getSubjectID());
+                dto.documentRoute.xdocumentdefinitionid = getParentRef().getParentrefKey();
                 dto.documentRoute.role = Convert.ToInt32(ddlEmailRouting.SelectedValue);
 
                 IResponseHandler response = new DocumentRouteSaveService().executeAsPrimary(dto);
