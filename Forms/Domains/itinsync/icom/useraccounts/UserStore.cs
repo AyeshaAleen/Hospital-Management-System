cba7b26836d3999d4b94b454efdb05e9412ed4e6
itinsync <services@itinsync.com>
2017-06-17 10:02:50 +0500

    sync

---
+++ Forms/Domains/itinsync/icom/useraccounts/UserStore.cs
@@ -0,0 +1,24 @@
+﻿using Domains.itinsync.interfaces.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.itinsync.icom.useraccounts
+{
+   public class UserStore : System.Attribute, IDomain
+    {
+        public enum columns { userid, storeid }
+        public enum primaryKey { userstoreid }
+        public Int32 userstoreid { get; set; }
+        public Int32 userid { get; set; }
+        public Int32 storeid { get; set; }
+        public object getKey() { return userstoreid; }
+
+        public void setTransID(object transID)
+        {
+
+        }
+    }
+}
