e31ec3124d8982ea75324bf2212abb353e246c82
Sohail <services@itinsync.com>
2017-07-07 14:23:01 +0500

    CSS Classes

--- Forms/Forms/itinsync/resources/pages/jquery.form-pickers.init.js
+++ Forms/Forms/itinsync/resources/pages/jquery.form-pickers.init.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function() {
                 jQuery('.datepicker-autoclose').datepicker({
                     format: "mm/dd/yyyy",
                     autoclose: true,
-                    showAnim: 'slideUp',
+                    endDate:'today',
                 	todayHighlight: true
                 });
                 jQuery('#datepicker-inline').datepicker();
